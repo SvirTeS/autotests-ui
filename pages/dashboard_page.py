d34d40d479a14de81e9cb35d88c7b67e3e1cf214
SvirTeS <svirtes@gmail.com>
2025-04-17 19:46:50 +0400

    update test and fix DashboardPage

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -2,12 +2,12 @@ from playwright.sync_api import Page, expect
 from pages.base_page import BasePage
 
 
-class DashboardPage:
+class DashboardPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
         self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
 
-    def visible_dashboard_title(self):
+    def check_visible_dashboard_title(self):
         expect(self.dashboard_title).to_be_visible()
         expect(self.dashboard_title).to_have_text("Dashboard")
